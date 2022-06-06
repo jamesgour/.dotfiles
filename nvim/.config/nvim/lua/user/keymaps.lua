33c9d068a3a9491f7b634dc2e0a9a6633577bbe5
James <james.c.gourley@gmail.com>
2022-06-06 09:54:15 -0700

    Added keymap for faster :noh

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -83,6 +83,9 @@ keymap("n", "<leader>cr", ":call CodeReview(1)<CR>", opts)
 -- Primeagen cht.sh tool https://www.youtube.com/watch?v=hJzqEAf2U4I
 keymap("n", "<leader>i", ":terminal ~/.dotfiles/bin/tmux-cht.sh<CR>", opts)
 
+-- Faster :noh
+keymap("n", "<leader>n", ":noh<CR>", opts)
+
 ---------- Insert Mode ----------
 -- Leave insert mode with "jk" instead of ESC
 keymap("i", "jk", "<ESC>", opts)
