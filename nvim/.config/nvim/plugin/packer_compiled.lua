70d319584ac70331b75e7b01b8d7104d6467edb5
James <james.c.gourley@gmail.com>
2022-07-08 10:58:16 -0700

    Continued working on Tmux flow

--- nvim/.config/nvim/plugin/packer_compiled.lua
+++ nvim/.config/nvim/plugin/packer_compiled.lua
@@ -254,11 +254,6 @@ _G.packer_plugins = {
     path = "/home/jamesgou/.local/share/nvim/site/pack/packer/start/vim-css-color",
     url = "https://github.com/ap/vim-css-color"
   },
-  ["vim-obsession"] = {
-    loaded = true,
-    path = "/home/jamesgou/.local/share/nvim/site/pack/packer/start/vim-obsession",
-    url = "https://github.com/tpope/vim-obsession"
-  },
   ["vim-repeat"] = {
     loaded = true,
     path = "/home/jamesgou/.local/share/nvim/site/pack/packer/start/vim-repeat",
